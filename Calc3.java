8050f3c7a07681b9299b40a704a10b137e706b9d
bxtr <bexter1475@gmail.com>
2023-01-07 13:13:50 +0900

    Servlet Calc

---
+++ Calc3.java
@@ -0,0 +1,51 @@
+package com.bexter.web;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/calc3")
+public class Calc3 extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Cookie[] cookies = req.getCookies();
+
+        String value = req.getParameter("value");
+        String operator = req.getParameter("operator");
+        String dot = req.getParameter("dot");
+
+        String exp =  "";
+        if(cookies != null){
+            for(Cookie c : cookies){
+                if(c.getName().equals("exp")) {
+                    exp = c.getValue();
+                    break;
+                }
+            }
+        }
+
+        if(operator != null && operator.equals("=")){
+            ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+            try {
+                exp = String.valueOf(engine.eval(exp));
+            } catch (ScriptException e) {
+                e.printStackTrace();
+            }
+        } else{
+            exp += (value==null)?"":value;
+            exp += (operator==null)?"":operator;
+            exp += (dot==null)?"":dot;
+        }
+
+        Cookie expCookie = new Cookie("exp", exp);
+            resp.addCookie(expCookie);
+            resp.sendRedirect("calcpage");
+    }
+}
